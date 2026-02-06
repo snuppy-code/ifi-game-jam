0257d35e457c7b8b8720c8d43eb40f87e55b7c44
snuppy <snuppy.code+git@pm.me>
2026-02-06 14:49:10 +0100

    clean dat up

--- flake.nix
+++ flake.nix
@@ -12,20 +12,15 @@
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     devShells."x86_64-linux".default = pkgs.mkShell {
-      packages = with pkgs; [(python311.withPackages (ps: [ps.pygame-ce]))];
+      #      # could also be buildInputs,, for my purposes it do not matter: https://discourse.nixos.org/t/difference-between-buildinputs-and-packages-in-mkshell/60598/2
+      packages = with pkgs; [
+        (python311.withPackages (ps:
+          with ps; [
+            pygame-ce
+          ]))
+      ];
     };
-
-    #pkgs.mkShell {
-    #      # could also be buildInputs,, for my purposes it do not matter: https://discourse.nixos.org/t/difference-between-buildinputs-and-packages-in-mkshell/60598/2
-    #      buildInputs = with pkgs; [
-    #        python315.withPackages
-    #        (ps: [
-    #          #pygame-ce
-    #          ps.numpy
-    #        ])
-    #      ];
-    #      # env variables..
-    #      #env.RUST_SRC_PATH = "${pkgs.rust.blablabla.rustLibSrc}";
-    #    };
+    #env variables..
+    #env.RUST_SRC_PATH = "${pkgs.rust.blablabla.rustLibSrc}";
   };
 }
